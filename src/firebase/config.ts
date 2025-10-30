447688479a10b68f9d25a209eb4c57078ae536a1
Christopher Nielsen <Kontakt@fm-service.no>
2025-10-30 12:49:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9850733688-32e54",
+  "appId": "1:765727711857:web:c1371e86e97251e3c731d0",
+  "apiKey": "AIzaSyBcWdDoUiA_9ipRK81BOSzxCAnPr4uZIiY",
+  "authDomain": "studio-9850733688-32e54.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "765727711857"
+};
