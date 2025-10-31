aa12b1ae9835a05196c9aa2bf7e2f180d7cf5dbe
Christopher Nielsen <Kontakt@fm-service.no>
2025-10-31 18:01:45 +0000

    12

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -289,7 +289,7 @@ export default function SignupPage() {
                                     render={({ field }) => (
                                         <FormItem>
                                             <FormLabel>Company Type</FormLabel>
-                                            <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                            <Select onValueChange={field.onChange} value={field.value}>
                                                 <FormControl>
                                                     <SelectTrigger>
                                                         <SelectValue placeholder="Select company type" />
