0b5597f50498597308b639e9526b7ef9f1a15fa3
Christopher Nielsen <Kontakt@fm-service.no>
2025-11-01 10:27:38 +0000

    In HTML, <html> cannot be a child of <div>. This will cause a hydration

--- src/app/signup/layout.tsx
+++ src/app/signup/layout.tsx
@@ -1,20 +1,4 @@
-import "../globals.css";
-import { cn } from "@/lib/utils";
-import { Toaster } from "@/components/ui/toaster";
-import { Poppins, PT_Sans } from 'next/font/google';
 import { FirebaseClientProvider } from "@/firebase/client-provider";
-
-const fontPoppins = Poppins({ 
-  subsets: ['latin'], 
-  variable: '--font-poppins',
-  weight: ['400', '600', '700'] 
-});
-const fontPtSans = PT_Sans({ 
-  subsets: ['latin'], 
-  variable: '--font-pt-sans',
-  weight: ['400', '700']
-});
-
 
 export default function SignupLayout({
   children,
@@ -22,22 +6,8 @@ export default function SignupLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-body antialiased",
-          fontPoppins.variable,
-          fontPtSans.variable
-        )}
-        suppressHydrationWarning={true}
-      >
-        <FirebaseClientProvider>
-          <div>
-            {children}
-            <Toaster />
-          </div>
-        </FirebaseClientProvider>
-      </body>
-    </html>
+    <FirebaseClientProvider>
+      {children}
+    </FirebaseClientProvider>
   );
 }
